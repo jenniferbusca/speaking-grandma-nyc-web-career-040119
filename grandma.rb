7cec2668a69e91f84cd746092b8a1adaa07c1b9c
Jennifer Busca <jennifer.busca@gmail.com>
2019-03-22 20:10:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,11 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return 
+    return "I LOVE YOU TOO PUMPKIN!"
+  else phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
